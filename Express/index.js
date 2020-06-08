d59706550fa3903342e4349bdba547cc2df075ab
SL Onstwedder <sebbie1991@hotmail.com>
2020-06-08 16:06:51 +0200

    init

---
+++ Express/index.js
@@ -0,0 +1,29 @@
+import express, { request } from 'express';
+import data from './data/MOCK_DATA.json'
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => 
+    // get data first
+    res.json(data)
+);
+
+app.post('/newItem', (req, res) => 
+    res.send(`a post request with /newItem route on port ${PORT}`)
+);
+
+app.put('/item', (req, res) => 
+    res.send(`a post request with /item route on port ${PORT}`)
+);
+
+app.delete('/item', (req, res) => 
+    res.send(`a delete request with /item route on port ${PORT}`)
+);
+
+app.listen(PORT, (() => {
+console.log(`Your server is running on port ${PORT}`);
+console.log(data);
+}));
